351551e2ca311a3b9727e89508b6971fafa14596
romain helou-lenoir <romain@snarkfactory.fr>
2021-09-30 11:50:54 +0200

    💈

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-(function () {
-    let likes = 0;
-    setInterval(() => {
-        const heart = document.querySelector('svg[aria-label="J\'aime"]').parentNode;
-        const arrow = document.querySelector('a.coreSpriteRightPaginationArrow');
-        if (heart) {
-            heart.click()
-            likes++;
-            console.log(`You've liked ${likes} post(s)`);
-        }
-        arrow.click();
-    }, getRandomArbitrary(5000, 15000)); 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
-    function getRandomArbitrary(min, max) {
-        return Math.random() * (max - min) + min;
+let likes = 0;
+setInterval(() => {
+    const heart = document.querySelector('svg[aria-label="J'aime"]').parentNode;
+    const arrow = document.querySelector('a.coreSpriteRightPaginationArrow');
+    if (heart) {
+        heart.click()
+        likes++;
+        console.log(`You've liked ${likes} post(s)`);
     }
-})();
+    arrow.click();
+}, getRandomArbitrary(5000, 15000)); 
